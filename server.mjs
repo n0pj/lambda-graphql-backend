f467edebdeb5c3ca97d531a4bdc4d071521e4b27
n0pj <admin@n0pj.com>
2023-04-12 11:39:32 +0900

    add server.js

--- server.mjs
+++ server.mjs
@@ -6,7 +6,7 @@ import { dirname } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const app = express()
-const port = 9325
+const port = 9324
 const reportPath = path.join(__dirname, './reports/jest-html-report')
 
 app.use(express.static(reportPath))
